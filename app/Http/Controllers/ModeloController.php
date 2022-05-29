cac15e2503789e383da3252edc983f8b9efc3af5
unknown <eduardo.farinango@epn.edu.ec>
2022-05-29 00:47:00 -0500

    Modelos-ok

--- app/Http/Controllers/ModeloController.php
+++ app/Http/Controllers/ModeloController.php
@@ -14,15 +14,62 @@ class ModeloController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $autos =
-        [
-            "CHEVROLET"=>'TRACKER',
-            "MAZDA"=>'323',
-            "FORD"=>'RANGER',
-            "KIA"=>'SPORTAGE',
-            "GREAT WALL"=>'WINGLE'
+        $autos =[
+
+            [
+                'Imagen' => 'https://www.nissan-cdn.net/content/dam/Nissan/ec/vehicles/Versa_MY20/launch/menu/versa-my20-thumb-menu.png.ximg.l_3_m.smart.png',
+                'Marca' => 'Mazda 6',
+                'Estado' => 'Usado',
+                'Web' => 'https://www.mazda.com.ec/'
+            ],
+            [
+                'Imagen' => 'https://www.nissan-cdn.net/content/dam/Nissan/ec/vehicles/leaf/overview/nissan-leaf-menu.png.ximg.l_3_m.smart.png',
+                'Marca' => 'Ford',
+                'Estado' => 'Nuevo',
+                'Web' => 'https://www.ford.com/es-co/'
+
+            ],
+            [
+                'Imagen' => 'https://www.nissan-cdn.net/content/dam/Nissan/ec/vehicles/frontier21/vehicles/frontier/MY21/VAP/Frontier-PRO-4X-menu.png.ximg.l_3_m.smart.png',
+                'Marca' => 'Suzuki',
+                'Estado' => 'Nuevo',
+                'Web' => 'https://www.ford.com/es-co/'
+
+            ],
+            [
+                'Imagen' => 'https://www.nissan-cdn.net/content/dam/Nissan/nissan_latin_america/vehicles/X%20Trail%20T32/Minor_Change/range/RangePage_1536x864_Xtrail.png.ximg.l_3_m.smart.png',
+                'Marca' => 'Suzuki',
+                'Estado' => 'Nuevo',
+                'Web' => 'https://www.ford.com/es-co/'
+
+            ],
+            [
+                'Imagen' => 'https://www.nissan-cdn.net/content/dam/Nissan/ec/vehicles/kicks1/my21/vap/nissan-2021-kicks_platinum_cvt_cafe_bi_ton-bitono_azul-negro-exterior-miniatura_lateral-i.jpg.ximg.l_3_m.smart.jpg',
+                'Marca' => 'Suzuki',
+                'Estado' => 'Nuevo',
+                'Web' => 'https://www.ford.com/es-co/'
+
+            ],
+            [
+                'Imagen' => 'https://www.nissan-cdn.net/content/dam/Nissan/ec/vehicles/kicks1/my21/vap/nissan-2021-kicks_platinum_cvt_cafe_bi_ton-bitono_azul-negro-exterior-miniatura_lateral-i.jpg.ximg.l_3_m.smart.jpg',
+                'Marca' => 'Suzuki',
+                'Estado' => 'Nuevo',
+                'Web' => 'https://www.ford.com/es-co/'
+
+            ]
+
+
+
         ];
-        $nombre = "Byron";
-    return view('modelos',compact('autos','nombre'));
+
+
+
+
+
+
+
+    return view('modelos',compact('autos'));
     }
 }
+
+
--- resources/views/modelos.blade.php
+++ resources/views/modelos.blade.php
@@ -3,44 +3,36 @@
 @section('title','Modelos')
 
 @section('content')
+
+
 <h1>Nuestros modelos 2022</h1>
-
-
-@for ($i = 0; $i < 10; $i++)
-    El valor actual es: {{ $i }}<br>
-@endfor
-
 <hr>
+<div class="row bg-image hover-zoom">
+    @foreach($autos as $auto)
+    <div class="col-md-4">
+        <div class="card" >
+            <img class="card-img-top img-fluid "  src="{{$auto['Imagen']}} " alt="Card image cap" >
+            <div class="card-body ">
+                <h5 class="card-title ">{{$auto['Marca']}}</h5>
+                <p class="card-text">{{$auto['Estado']}}</p>
+                <a class=" link-secondary py-3 d-block text-decoration-none text-center" href="{{$auto['Web']}}">Página Oficial</a>
 
-<ul>
-@foreach ($autos as $auto)
-    <li> Modelo: <b>{{ $auto }}</b></li>
-@endforeach
-</ul>
-
-
-<hr>
-
-<ol>
-@forelse($autos as $clave => $auto )
-    <li><b> {{$clave}} </b></li>
-     <ul>
-         <li>{{$auto}}</li>
-     </ul>
-@empty
-    <p>No Existen autos disponibles</p>
-@endforelse
-</ol>
+            </div>
+        </div>
+        <hr >
+    </div>
+    @endforeach
 
 
-<hr>
-@if (count($autos) === 1)
-    Existe un registro
-@elseif (count($autos) > 1)
-   Existen múltiples registros!
-@else
-    No existen registros!
-@endif
+
+
+
+
+
+
+
+
+
 
 
 
